687c25246fe2512e7e51cbde680c37d3d0b20b66
SHAKIL AHAMMED <shakilahammed01234@gmail.com>
2024-08-18 17:26:47 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let main_click = () => {
+    let inputField = document.getElementById("inputField");
+    let result = document.querySelector(".result");
+    let btn = document.getElementById("btn");
+
+    let inputMain = inputField.value;
+
+    if (inputMain == "") {
+        result.textContent = "Please enter a number";
+    }
+    else if (inputMain == 0) {
+        result.textContent = "Its a ZERO ";
+
+    }
+    else if (inputMain > 0) {
+        result.textContent = "Its a Positive value ";
+
+    } else if (inputMain < 0) {
+        result.textContent = "Its a Positive value ";
+
+    } else {
+        result.textContent = "not a number"
+
+    }
+
+}
+
+btn.addEventListener("click", () => {
+    main_click();
+    setTimeout(() => {
+        inputField.value = "";
+
+    }, 500)
+})
